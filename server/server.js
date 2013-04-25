576cf38b4e95750c7d729da1293b828ad5087cdb
Jollen Chen <jollen@moko365.com>
2013-04-25 10:25:10 +0800

    Modify: how to send HTTP response

--- server/server.js
+++ server/server.js
@@ -3,6 +3,12 @@ var http = require("http");
 function start() {
   function onRequest(request, response) {
     console.log("Request received.");
+
+    response.writeHead(200, {
+    	'Content-Type': 'text/html'
+    });
+
+    response.end('<h1>Hello World</h1>');
   }  
 
   var server = http.createServer(onRequest).listen(8888);
